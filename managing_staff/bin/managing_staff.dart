1366a33f6c8e10b1e65bd42595e9f1b1b9f102c6
XRockKimu <kimkheng.ly@student.cadt.edu.kh>
2025-10-31 13:03:47 +0700

    First Commit

---
+++ managing_staff/bin/managing_staff.dart
@@ -0,0 +1,5 @@
+import 'package:managing_staff/managing_staff.dart' as managing_staff;
+
+void main(List<String> arguments) {
+  print('Hello world: ${managing_staff.calculate()}!');
+}
---
+++ managing_staff/test/managing_staff_test.dart
@@ -0,0 +1,8 @@
+import 'package:managing_staff/managing_staff.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
